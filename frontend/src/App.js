c9228391ac0f9002346190d9f6c82faf05fbe6a9
Harvey Zhu <zhuharvey@gmail.com>
2024-05-14 17:12:29 -0400

    changed to a time limit format

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import fetchWords from './services/WordService';
 import Word from './components/Word';
 import Timer from './components/Timer';
+import wordsData from './words/English.json'
 
 import './App.css';
 
@@ -25,11 +26,45 @@ function App() {
   const [missingChars, setMissingChars] = useState(0);
   const [extraChars, setExtraChars] = useState(0);
 
+  const [timeLimit, setTimeLimit] = useState(30) // default set to 30 seconds
+  const [timeLeft, setTimeLeft] = useState(timeLimit)
+
   useEffect(() => {
-    fetchWords().then(words => {
-      setWordBox(words.split(' '));
-    })
-  }, [])
+      let timer = null;
+      if (startCounting && timeLeft > 0) {
+          timer = setTimeout(() => {
+              setTimeLeft(timeLeft - 1);
+          }, 1000);
+      } else if (timeLeft === 0) {
+          setIsTestOver(true);
+          setStartCounting(false);
+      }
+      return () => clearTimeout(timer);
+  }, [startCounting, timeLeft]);
+
+
+  useEffect(() => {
+    setWordBox(shuffleArray([...wordsData.words]));
+  }, []);
+
+  function handleTimeLimitChange(event) {
+    const newTimeLimit = parseInt(event.target.value, 10);
+    setTimeLimit(newTimeLimit);
+    setTimeLeft(newTimeLimit);
+  }
+
+  function getRandomWord() {
+    return wordsData.words[Math.floor(Math.random() * wordsData.words.length)];
+  }
+
+  // function to initially shuffle the word box
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    }
+    return array;
+  }
 
   const handleUserInput = (inputValue) => {
     // check if test has ended, if so do nothing
@@ -40,15 +75,10 @@ function App() {
       setStartCounting(true);
     }
 
-    // console.log(wordBox)
-    // console.log(wordBox[currentWordIndex])
-    // console.log(previousWords)
-    // console.log(currentWordIndex)
-
-    // Update user input
     setUserInput(inputValue);
 
     // makes it so that backspace isn't counted as a character
+    // total chars is calculated with every keystroke -> might be inefficient
     setTotalChars((prev) => prev + inputValue.length - userInput.length)
 
     // check if the last character is a space and that there's more than just spaces
@@ -102,13 +132,21 @@ function App() {
         }
     }]);
 
-      if (currentWordIndex === wordBox.length - 1) {
-        setIsTestOver(true);
-        setStartCounting(false);
-      } else {
-          setCurrentWordIndex(index => index + 1);
-      }
-      setUserInput('');
+    if (!isTestOver) {
+      setCurrentWordIndex(currentWordIndex + 1)
+      setWordBox([...wordBox, getRandomWord()])
+    }
+
+    setUserInput('')
+
+
+    // if (currentWordIndex === wordBox.length - 1) {
+    //   setIsTestOver(true);
+    //   setStartCounting(false);
+    // } else {
+    //     setCurrentWordIndex(index => index + 1);
+    // }
+    // setUserInput('');
     
     }
 
@@ -126,6 +164,14 @@ function App() {
           extraChars={extraChars}
           totalChars={totalChars}
       />
+
+      <p>Time left: {timeLeft} seconds</p>
+
+      <select onChange={handleTimeLimitChange} value={timeLimit}>
+          <option value={10}>10 seconds</option>
+          <option value={15}>15 seconds</option>
+          <option value={30}>30 seconds</option>
+      </select>
 
       <div className='typing-area'>
       {wordBox.map((word, index) => (
