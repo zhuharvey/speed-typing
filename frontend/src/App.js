f9cd7970aa0f8278481ae01b4fd8389d59c8b268
Harvey Zhu <zhuharvey@gmail.com>
2024-05-25 01:30:21 -0400

    Revert "Merge branch 'word-rows'"
    This reverts commit 3a37a3986b9139685e93536570b07ca647897643, reversing
    changes made to e564e777fb60835ab5a21a3f724f9cae15cab340.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import fetchWords from './services/WordService';
 import Word from './components/Word';
 import Timer from './components/Timer';
 import wordsData from './words/English.json'
@@ -9,20 +10,20 @@ function App() {
 
   // in what cases do you need to use [] and in what cases do you not need to?
   const [userInput, setUserInput] = useState('')
-  const [rows, setRows] = useState([]) // array of word rows
-  const [currentRowIndex, setCurrentRowIndex] = useState(0) // index of current row
-  const [currentWordIndex, setCurrentWordIndex] = useState(0) // index of current word in current row  
-  const [startCounting, setStartCounting] = useState(false); // state to start the timer
-  const [isTestOver, setIsTestOver] = useState(false);
-  const [timeLimit, setTimeLimit] = useState(30); // default set to 30 seconds
-  const [timeLeft, setTimeLeft] = useState(timeLimit);
+  const [wordBox, setWordBox] = useState([])
+  const [startCounting, setStartCounting] = useState(false)
+  const [currentWordIndex, setCurrentWordIndex] = useState(0)
+  const [previousWords, setPreviousWords] = useState([]) // state to hold history of previous words
+  const [isTestOver, setIsTestOver] = useState(false)
   
-  const [previousWords, setPreviousWords] = useState([]); // state to hold history of previous words
   const [totalChars, setTotalChars] = useState(0)
   const [correctChars, setCorrectChars] = useState(0);
   const [incorrectChars, setIncorrectChars] = useState(0);
   const [missingChars, setMissingChars] = useState(0);
   const [extraChars, setExtraChars] = useState(0);
+
+  const [timeLimit, setTimeLimit] = useState(30) // default set to 30 seconds
+  const [timeLeft, setTimeLeft] = useState(timeLimit)
 
   useEffect(() => {
       let timer = null;
@@ -39,42 +40,46 @@ function App() {
 
 
   useEffect(() => {
-    initializeRows();
+    setWordBox(shuffleArray([...wordsData.words]));
   }, []);
 
-  const initializeRows = () => {
-    setRows([generateRow(), generateRow()]);
-  };
-
-  const generateRow = () => {
-    return Array.from({ length: 10 }, () => getRandomWord());
-  };
-
-  const getRandomWord = () => {
-    return wordsData.words[Math.floor(Math.random() * wordsData.words.length)];
-  };
-
-  const handleTimeLimitChange = (event) => {
+  function handleTimeLimitChange(event) {
     const newTimeLimit = parseInt(event.target.value, 10);
     setTimeLimit(newTimeLimit);
     setTimeLeft(newTimeLimit);
-  };
+  }
+
+  function getRandomWord() {
+    return wordsData.words[Math.floor(Math.random() * wordsData.words.length)];
+  }
+
+  // function to initially shuffle the word box
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+    }
+    return array;
+  }
 
   const handleUserInput = (inputValue) => {
     // check if test has ended, if so do nothing
     if(isTestOver) return;
 
     // Start the timer as soon as the user starts typing
-    if (!startCounting) setStartCounting(true);
+    if (!startCounting) {
+      setStartCounting(true);
+    }
 
     setUserInput(inputValue);
 
-    setTotalChars((prev) => prev + inputValue.length - userInput.length);
+    // makes it so that backspace isn't counted as a character
+    // total chars is calculated with every keystroke -> might be inefficient
+    setTotalChars((prev) => prev + inputValue.length - userInput.length)
 
     // check if the last character is a space and that there's more than just spaces
     if(inputValue.endsWith(' ') && inputValue.trim() !== '') {
-      const currentWords = rows[currentRowIndex]
-      const currentWord = currentWords[currentWordIndex]
+      const currentWord = wordBox[currentWordIndex]
       const typedWord = inputValue.trim()
       const maxLen = Math.max(currentWord.length, typedWord.length)
 
@@ -85,84 +90,103 @@ function App() {
 
       for (let i = 0; i < maxLen; i++) {
         if (i < typedWord.length && i < currentWord.length) {
-          if (typedWord[i] === currentWord[i]) {
-            tempCorrect++;
-          } else {
-            tempIncorrect++;
-          }
+            if (typedWord[i] === currentWord[i]) {
+                tempCorrect++
+            } else {
+                tempIncorrect++
+            }
         } else if (i >= typedWord.length) {
-          tempMissing++;
+            tempMissing++
         } else if (i >= currentWord.length) {
-          tempExtra++;
+            tempExtra++
         }
       }
 
-      // Update character states
-      setCorrectChars((prev) => prev + tempCorrect)
-      setIncorrectChars((prev) => prev + tempIncorrect)
-      setMissingChars((prev) => prev + tempMissing)
-      setExtraChars((prev) => prev + tempExtra)
+      // if word is correct and a space is pressed, include the space as a correct character
+      if (typedWord === currentWord) {
+        tempCorrect++
+      }
 
-      // Update previousWords state
+      // set character states
+      setCorrectChars((prev) => prev + tempCorrect);
+      setIncorrectChars((prev) => prev + tempIncorrect);
+      setExtraChars((prev) => prev + tempExtra);
+      setMissingChars((prev) => prev + tempMissing);
+
+      // determine is word is correct
+      const isCorrect = typedWord === currentWord;
+
+      // Update the previous words state with detailed information
       setPreviousWords(prev => [...prev, {
-        word: currentWord,
-        correct: typedWord === currentWord,
+        word: currentWord + ' ',
+        correct: isCorrect,
         details: {
             correct: tempCorrect,
             incorrect: tempIncorrect,
             missing: tempMissing,
             extra: tempExtra
         }
-      }]);
+    }]);
 
-      if (currentWordIndex === currentWords.length - 1) {
-        if (currentRowIndex < rows.length - 1) {
-          setCurrentRowIndex(currentRowIndex + 1);
-        } else {
-          setIsTestOver(true);
-          setStartCounting(false);
-        }
-        setCurrentWordIndex(0);
-        setRows((prevRows) => [...prevRows.slice(1), generateRow()]);
-      } else {
-        setCurrentWordIndex(currentWordIndex + 1);
-      }
-
-      setUserInput('');
+    if (!isTestOver) {
+      setCurrentWordIndex(currentWordIndex + 1)
+      setWordBox([...wordBox, getRandomWord()])
     }
-  };
 
+    setUserInput('')
+
+
+    // if (currentWordIndex === wordBox.length - 1) {
+    //   setIsTestOver(true);
+    //   setStartCounting(false);
+    // } else {
+    //     setCurrentWordIndex(index => index + 1);
+    // }
+    // setUserInput('');
+    
+    }
+
+
+  };
 
   return (
     <div>
       <h1>Typing Test</h1>
-      <Timer startCounting={startCounting} timeLeft={timeLeft} />
+      <Timer 
+          startCounting={startCounting}
+          correctChars={correctChars}
+          incorrectChars={incorrectChars}
+          missingChars={missingChars}
+          extraChars={extraChars}
+          totalChars={totalChars}
+      />
+
+      <p>Time left: {timeLeft} seconds</p>
+
       <select onChange={handleTimeLimitChange} value={timeLimit}>
-        <option value={10}>10 seconds</option>
-        <option value={15}>15 seconds</option>
-        <option value={30}>30 seconds</option>
+          <option value={10}>10 seconds</option>
+          <option value={15}>15 seconds</option>
+          <option value={30}>30 seconds</option>
       </select>
+
       <div className='typing-area'>
-        {rows.map((rowWords, idx) => (
-          <div key={idx} className="word-row">
-            {rowWords.map((word, wordIdx) => (
-              <Word
-                key={wordIdx}
-                text={word}
-                active={idx === 0 && wordIdx === currentWordIndex}
-                correct={idx === 0 && wordIdx < currentWordIndex} // Adjust based on your logic
-              />
-            ))}
-          </div>
-        ))}
+      {wordBox.map((word, index) => (
+          <Word
+              key={index}
+              text={word}
+              active={index === currentWordIndex}
+              correct={previousWords[index]?.correct}  // Pass the correct status from previousWords state
+          />
+      ))}
       </div>
+      
       <input
-        placeholder={isTestOver ? 'Test over, click to restart' : 'Start typing...'}
-        type="text"
-        value={userInput}
-        onChange={(e) => handleUserInput(e.target.value)}
-        autoFocus
-        disabled={isTestOver}
+          placeholder={isTestOver ? 'Test over, click to restart' : 'Start typing...'}
+          type="text"
+          value={userInput}
+          onChange={(e) => handleUserInput(e.target.value)}
+          autoFocus
+          disabled={isTestOver}
       />
       {isTestOver && (
         <div>
